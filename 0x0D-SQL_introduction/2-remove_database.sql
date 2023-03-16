d539975887d5080a43a239e27d3d8597ee5e3a2c
emmanueldavids <emmanueldavids417@gmail.com>
2023-03-16 13:20:05 +0000

    delete's a database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,6 @@
+-- a script that deletes the database 
+-- hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 does not exists, 
+-- your script should not fail
+
+DELETE DATABASE IF EXISTS hbtn_0c_0; 
