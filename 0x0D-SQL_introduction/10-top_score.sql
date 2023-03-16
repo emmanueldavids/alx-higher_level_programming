17ab17ba8c9b965c2631bf0fb77e0aa801930fd1
emmanueldavids <emmanueldavids417@gmail.com>
2023-03-16 16:04:41 +0000

    list of records of data in descending order

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -3,3 +3,5 @@
 --Results should display both the score and the name (in this order)
 --Records should be ordered by score (top first)
 --The database name will be passed as an argument of the mysql command
+
+SELECT score, name FROM second_table ORDER BY score DESC
