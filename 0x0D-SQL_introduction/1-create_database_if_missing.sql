255308419740903108a5137a682c2bbe77bcc0c5
emmanueldavids <emmanueldavids417@gmail.com>
2023-03-16 06:06:53 +0000

    creating a new database if not exits

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+-- a script that creates the database 
+-- hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 already exists, 
+-- your script should not fail
+
+CREATE DATABASE hbtn_0c_0 IF NOT EXITS; 
