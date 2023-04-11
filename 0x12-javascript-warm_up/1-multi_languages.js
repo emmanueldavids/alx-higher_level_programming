6ceca6fb05c497f0820bfec0cc4801ea1f7fb0e3
emmanueldavids <emmanueldavids417@gmail.com>
2023-04-11 15:46:14 +0000

    prints a number depending of the number of args passed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let firstline = "C is fun"
-let secondline = "Python is cool"
-let thirdline = "JavaScript is amazing"
-console.log(firstline+"\n"+secondline+"\n"+thirdline);
+let firstline = 'C is fun'
+let secondline = 'Python is cool'
+let thirdline = 'JavaScript is amazing'
+console.log(firstline+'\n'+secondline+'\n'+thirdline);
