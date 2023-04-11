57b1cfc0590a35c80ae0c4d62990d602e2b2e911
emmanueldavids <emmanueldavids417@gmail.com>
2023-04-11 12:44:46 +0000

    script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const firstline = "C is fun"
+const secondline = "Python is cool"
+const thirdline = "JavaScript is amazing"
+console.log(firstline+"\n"+secondline+"\n"+thirdline);
