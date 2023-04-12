e8249c9e9ebaa444520d536ee961f74dfae6dbd4
emmanueldavids <emmanueldavids417@gmail.com>
2023-04-12 13:16:02 +0000

    value of arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-const argc = process.argv.length;
-
-if (argc > 2) {
-  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
-} else {
-  console.log('No argument');
-}
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
