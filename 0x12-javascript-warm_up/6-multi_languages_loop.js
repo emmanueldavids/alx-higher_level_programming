a51b6ddf7292f791a6c154d92eda25c4243b3b3a
emmanueldavids <emmanueldavids417@gmail.com>
2023-04-12 13:47:37 +0000

    using array in js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-const myArray = ['C is fun' , 'Python is cool' , 'JavaScript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
 myArray.forEach((element) => {
     console.log(element);
 });
