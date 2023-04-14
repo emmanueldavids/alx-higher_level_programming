9bf046eadd97d3f9b8afbb9599838bac0b6ae14c
emmanueldavids <emmanueldavids417@gmail.com>
2023-04-14 16:36:45 +0000

    reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const revcount = [];
-  
-    for (let i = list.length - 1; i >= 0; i--) {
-      revcount.push(list[i]);
-    }
-  
-    return revcount;
+   const revcount = [];
+   for (let i = list.length - 1; i >= 0; i--) {
+    revcount.push(list[i]);
+}
+return revcount;
 };
