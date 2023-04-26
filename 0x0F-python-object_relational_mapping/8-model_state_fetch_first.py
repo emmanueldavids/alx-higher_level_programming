513907ca56bb17ba19c50f0d6a6216261f879bdf
emmanueldavids <emmanueldavids417@gmail.com>
2023-04-26 01:49:54 +0000

    Solutions

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
-"""
-Script that lists all State objects from the database hbtn_0e_6_usa
+""" prints the first State object from the database hbtn_0e_6_usa
 """
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.
-        format(mysql_username, mysql_password, database_name),
-        pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    first_state = session.query(State).order_by(State.id).first()
-
-    if first_state is None:
+    instance = session.query(State).first()
+    if instance is None:
         print("Nothing")
     else:
-        print("{}: {}".format(first_state.id, first_state.name))
-    
-    
-    session.close()
+        print(instance.id, instance.name, sep=": ")
