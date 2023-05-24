c43270c57ccba1a6f51726a3b91aa28527082870
emmanueldavids <emmanueldavids417@gmail.com>
2023-05-24 13:02:39 +0000

    writing into a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-// const content = "Python is cool!"
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
-    if(err){
-        console.error(err);
-        return;
-    }
-    
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
